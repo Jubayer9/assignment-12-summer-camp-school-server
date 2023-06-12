c89eef48698091dabea343c30715dfd0d52df8ab
Jubayer9 <121636287+Jubayer9@users.noreply.github.com>
2023-06-12 13:27:56 +0600

    vercel config file

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
-        
+
         const classCollection = client.db("bandZoon").collection("classes")
         const paymentCollection = client.db("bandZoon").collection("payments")
         const instructorCollection = client.db("bandZoon").collection("Instructor")
